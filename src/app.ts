8b8ac997ee25c29ab0e86118168ebdf121afa6bb
JerinXplore <jerinjerin101325@gmail.com>
2025-12-29 13:04:57 +0600

    Implemented Better Auth authentication for email and password

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express, { Application } from "express";
 import { postRouter } from "./lib/modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app:Application = express();
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.use(express.json());
 app.use("/posts",postRouter);
 app.get("/",(req,res)=>{
