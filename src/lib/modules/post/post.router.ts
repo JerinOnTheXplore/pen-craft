c6e6c87ed263ba8fe46d68dc210b8544fc8f9317
JerinXplore <jerinjerin101325@gmail.com>
2025-12-31 02:41:28 +0600

    created role based access control middleware

--- src/lib/modules/post/post.router.ts
+++ src/lib/modules/post/post.router.ts
@@ -1,20 +1,69 @@
 import express, {NextFunction, Request, Response, Router} from 'express';
 import { PostController } from './post.controller';
 import {auth as betterAuth} from '../../auth'
+import { success } from 'better-auth/*';
 const router = express.Router();
 
-const auth = (...roles: any) => {
+export enum UserRole {
+    USER = "USER",
+    ADMIN = "ADMIN"
+}
+
+declare global {
+    namespace Express {
+        interface Request{
+            user?:{
+                id: string;
+                email: string;
+                name: string;
+                role: string;
+                emailVerified : boolean
+            }
+        }
+    }
+}
+
+const auth = (...roles: UserRole[]) => {
     return async (req: Request, res: Response,next: NextFunction) => {
         const session = await betterAuth.api.getSession({
             headers: req.headers as any
         })
+
+        if(!session){
+            return res.status(401).json({
+                success:false,
+                message: "You are not authorized!"
+            })
+        }
+        if(!session.user.emailVerified){
+            return res.status(403).json({
+                success:false,
+                message: "Email verification required. Please verify your email"
+            })
+        }
+
+        req.user = {
+            id: session.user.id,
+            email: session.user.email,
+            name: session.user.name,
+            role: session.user.role as string,
+            emailVerified: session.user.emailVerified
+        }
+
+        if(roles.length && !roles.includes(req.user.role as UserRole)){
+            return res.status(403).json({
+                success: false,
+                message: "Forbidden ! You don't have permission to access this resources!"
+            })
+        }
+        next()
         console.log(session);
     }
 }
 
 router.post(
     "/",
-    auth("ADMIN","USER"),
+    auth(UserRole.USER),
     PostController.createPost
 )
 
