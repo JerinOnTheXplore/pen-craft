cf903c173e4a66b127e08e8e4ee376c7c4b5de55
JerinXplore <jerinjerin101325@gmail.com>
2025-12-30 21:45:27 +0600

    Implemented social authentication:Sign Up and Sign In with Google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -136,4 +136,11 @@ export const auth = betterAuth({
       }
     },
   },
+
+   socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
 });
